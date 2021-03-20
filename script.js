44d364da0e54261b4355009bf4114aabf6be6f25
danielLoaiza <cohl.daniel@gmail.com>
2021-03-20 16:07:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ var panuelo = new THREE.Object3D();
 var cant = 20;
 var radio = 70;
 var lista = [
-    {lt:-34.920979,lg:-57.941662},
-    {lt:-34.914454,lg:-57.946792}    
+    {lt:-34.914454,lg:-57.946792},
+    {lt:-34.920979,lg:-57.941662}       
 ];
 var poss = [
   new THREE.Vector2(0, 0),
